abe980930dc35a546f945bf8fcb7b61ca4e7a770
Mohan <moveraitch@gmail.com>
2024-06-20 21:34:36 +0100

    working time seriod

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
-import nasdaq_scrapper
+import json
+from datetime import datetime
+
+
+# import nasdaq_scrapper
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -83,14 +87,19 @@ def search():
         # time_period = request.form['time-period']
         stock_info = yf.Ticker(stock_symbol)
         stock_data = stock_info.info
-        
-        # Fetch historical stock data
-        stock_history = stock_info.history(period="6mo")
 
-        # Create the stock price graph
-        trace = go.Scatter(x=stock_history.index,
-                            y=stock_history['Close'],
-                            mode='lines')
+         # Fetch historical stock data for all time periods
+        stock_history_1d = stock_info.history(period='1d', interval='5m').reset_index().to_dict('records')
+        stock_history_5d = stock_info.history(period='5d', interval='1d').reset_index().to_dict('records')
+        stock_history_1mo = stock_info.history(period='1mo').reset_index().to_dict('records')
+        stock_history_6mo = stock_info.history(period='6mo').reset_index().to_dict('records')
+        stock_history_1y = stock_info.history(period='1y').reset_index().to_dict('records')
+        stock_history_max = stock_info.history(period='max').reset_index().to_dict('records')
+
+        # Create the stock price graph for the default time period (6mo)
+        trace = go.Scatter(x=[data['Date'] for data in stock_history_6mo],
+                           y=[data['Close'] for data in stock_history_6mo],
+                           mode='lines')
         layout = go.Layout(title=f"{stock_data['longName']} Stock Price",
                            xaxis_title="Date",
                            yaxis_title="Price (USD)")
@@ -102,8 +111,13 @@ def search():
         # print(stock_data)
         news_articles = newsapi.get_everything(q=news_query, language='en', sort_by='publishedAt')['articles'][:4]
         # news_articles = [article for article in newsapi.get_everything(q=news_query, sort_by='publishedAt')['articles'] if 'language' in article and article['language'] == 'en'][:4]
-        
-        return render_template('stock_info.html', stock_data=stock_data, graph_div=graph_div, news_articles=news_articles, is_search=True)
+        # Convert stock_history to JSON
+        # stock_history_json = json.dumps(stock_history)
+        return render_template('stock_info.html', stock_data=stock_data, graph_div=graph_div, news_articles=news_articles,
+                                stock_history_1d=stock_history_1d, stock_history_5d=stock_history_5d,
+                                stock_history_1mo=stock_history_1mo, stock_history_6mo=stock_history_6mo,
+                                stock_history_1y=stock_history_1y, stock_history_max=stock_history_max, 
+                                is_search=True)
     return render_template('index.html')
 
 if __name__ == '__main__':
